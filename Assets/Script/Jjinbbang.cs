f1fb335ad4acd270881999d7c2296a783b2d9e13
zollahungry <zollahungry@naver.com>
2018-03-17 17:38:59 +0900

    더미데이터로 덫구 기본, 걷기 애니메이션 추가하고 Jjinbbang, JBAnimationControl 스크립트 생성 이동 구현

---
+++ Assets/Script/Jjinbbang.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Jjinbbang : MonoBehaviour
+{
+    [SerializeField] public Transform _skin;
+    public JBAnimationControl AnimControl { get { return _anim; } }
+
+    JBAnimationControl _anim;
+
+    public float moveSpeed;
+    public bool gameOver;
+
+    void Initialize()
+    {
+        moveSpeed = 4f;
+        gameOver = false;
+    }
+
+    void Start()
+    {
+        _anim = _skin.GetComponent<JBAnimationControl>();
+        _skin = transform.Find("skin");
+        Initialize();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (!gameOver)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            transform.Translate(Vector2.right * moveX * moveSpeed * Time.deltaTime);
+            float moveY = Input.GetAxis("Vertical");
+            transform.Translate(Vector2.up * moveY * moveSpeed * Time.deltaTime);
+
+            if(moveX == 0 && moveY == 0)
+            {
+                _anim.animator.SetBool("walking", false);
+
+            }
+
+            if (moveX < 0)
+            {
+                _anim.animator.SetBool("walking", true);
+                _anim.sprite.flipX = false;
+            }
+            else if(moveX > 0)
+            {
+                _anim.animator.SetBool("walking", true);
+                _anim.sprite.flipX = true;
+            }
+        }
+    }
+}
