46a43fb62d0b4526cd27a49dab43b9d2eb9090dc
zollahungry <zollahungry@naver.com>
2018-04-02 02:35:42 +0900

    찐빵 이동 스크립트 수정

--- Assets/Script/Jjinbbang.cs
+++ Assets/Script/Jjinbbang.cs
@@ -13,53 +13,111 @@ public class Jjinbbang : MonoBehaviour
     public bool gameOver;
 
     public LayerMask layerMask;
+    public Rigidbody2D rigid;
     Ray ray;
     RaycastHit2D hit;
 
     void Initialize()
     {
-        moveSpeed = 4f;
+        moveSpeed = 300f;
         gameOver = false;
     }
 
     void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
         _anim = _skin.GetComponent<JBAnimationControl>();
         _skin = transform.Find("skin");
         Initialize();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
 
     void Move()
     {
-        if (!gameOver && !RayCheck(/*방향*/))
+        Vector2 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            float moveX = Input.GetAxis("Horizontal");
-            transform.Translate(Vector2.right * moveX * moveSpeed * Time.deltaTime);
-            float moveY = Input.GetAxis("Vertical");
-            transform.Translate(Vector2.up * moveY * moveSpeed * Time.deltaTime);
-
-            if(moveX == 0 && moveY == 0)
+            moveVelocity = Vector2.left;
+            _anim.sprite.flipX = false;
+            rigid.velocity = Vector2.zero;
+            if (Input.GetAxisRaw("Vertical") < 0)
             {
-                _anim.animator.SetBool("walking", false);
-
+                moveVelocity += Vector2.down;
+                rigid.velocity = Vector2.zero;
             }
-
-            if (moveX < 0)
+            else if (Input.GetAxisRaw("Vertical") > 0)
             {
-                _anim.animator.SetBool("walking", true);
-                _anim.sprite.flipX = false;
-            }
-            else if(moveX > 0)
-            {
-                _anim.animator.SetBool("walking", true);
-                _anim.sprite.flipX = true;
+                moveVelocity += Vector2.up;
+                rigid.velocity = Vector2.zero;
             }
         }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector2.right;
+            _anim.sprite.flipX = true;
+            rigid.velocity = Vector2.zero;
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                moveVelocity += Vector2.down;
+                rigid.velocity = Vector2.zero;
+            }
+            else if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                moveVelocity += Vector2.up;
+                rigid.velocity = Vector2.zero;
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            moveVelocity += Vector2.down;
+            rigid.velocity = Vector2.zero;
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                moveVelocity += Vector2.left;
+                _anim.sprite.flipX = false;
+                rigid.velocity = Vector2.zero;
+            }
+            else if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                moveVelocity += Vector2.right;
+                _anim.sprite.flipX = true;
+                rigid.velocity = Vector2.zero;
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            moveVelocity += Vector2.up;
+            rigid.velocity = Vector2.zero;
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                moveVelocity += Vector2.left;
+                _anim.sprite.flipX = false;
+                rigid.velocity = Vector2.zero;
+            }
+            else if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                moveVelocity += Vector2.right;
+                _anim.sprite.flipX = true;
+                rigid.velocity = Vector2.zero;
+            }
+        }
+
+        if (moveVelocity.x == 0 && moveVelocity.y == 0)
+        {
+            _anim.animator.SetBool("walking", false);
+            rigid.velocity = Vector2.zero;
+        }
+        else
+            _anim.animator.SetBool("walking", true);
+
+
+        rigid.AddForce(moveVelocity.normalized * moveSpeed);
     }
 
     bool RayCheck(Vector2 JBdirection)
