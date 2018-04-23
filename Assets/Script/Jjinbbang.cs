8f734c326c07f7b00cd64a75111644fb09edd93a
zollahungry <zollahungry@naver.com>
2018-04-24 00:51:30 +0900

    찐빵 스크립트 이동 부분 깔끔하고 짧게 수정

--- Assets/Script/Jjinbbang.cs
+++ Assets/Script/Jjinbbang.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 
 public class Jjinbbang : MonoBehaviour
 {
-    [SerializeField] public Transform _skin;
+    [SerializeField]
+    public Transform _skin;
     public JBAnimationControl AnimControl { get { return _anim; } }
 
     JBAnimationControl _anim;
@@ -14,12 +15,16 @@ public class Jjinbbang : MonoBehaviour
 
     public LayerMask layerMask;
     public Rigidbody2D rigid;
+    public Vector2 moveInput;
+
+    Vector2 moveVelocity;
     Ray ray;
     RaycastHit2D hit;
 
     void Initialize()
     {
-        moveSpeed = 300f;
+        moveSpeed = 3f;
+        moveVelocity = Vector3.zero;
         gameOver = false;
     }
 
@@ -38,77 +43,11 @@ public class Jjinbbang : MonoBehaviour
 
     void Move()
     {
-        Vector2 moveVelocity = Vector3.zero;
 
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            moveVelocity = Vector2.left;
-            _anim.sprite.flipX = false;
-            rigid.velocity = Vector2.zero;
-            if (Input.GetAxisRaw("Vertical") < 0)
-            {
-                moveVelocity += Vector2.down;
-                rigid.velocity = Vector2.zero;
-            }
-            else if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                moveVelocity += Vector2.up;
-                rigid.velocity = Vector2.zero;
-            }
-        }
-        else if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            moveVelocity = Vector2.right;
-            _anim.sprite.flipX = true;
-            rigid.velocity = Vector2.zero;
-            if (Input.GetAxisRaw("Vertical") < 0)
-            {
-                moveVelocity += Vector2.down;
-                rigid.velocity = Vector2.zero;
-            }
-            else if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                moveVelocity += Vector2.up;
-                rigid.velocity = Vector2.zero;
-            }
-        }
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            moveVelocity += Vector2.down;
-            rigid.velocity = Vector2.zero;
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                moveVelocity += Vector2.left;
-                _anim.sprite.flipX = false;
-                rigid.velocity = Vector2.zero;
-            }
-            else if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                moveVelocity += Vector2.right;
-                _anim.sprite.flipX = true;
-                rigid.velocity = Vector2.zero;
-            }
-        }
-        else if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            moveVelocity += Vector2.up;
-            rigid.velocity = Vector2.zero;
-            if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                moveVelocity += Vector2.left;
-                _anim.sprite.flipX = false;
-                rigid.velocity = Vector2.zero;
-            }
-            else if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                moveVelocity += Vector2.right;
-                _anim.sprite.flipX = true;
-                rigid.velocity = Vector2.zero;
-            }
-        }
-
-        if (moveVelocity.x == 0 && moveVelocity.y == 0)
+        // 물리의 속도값으로 이동 애니메이션 실행여부 결정
+        if (rigid.velocity.x == 0 && rigid.velocity.y == 0)
         {
             _anim.animator.SetBool("walking", false);
             rigid.velocity = Vector2.zero;
@@ -116,8 +55,14 @@ public class Jjinbbang : MonoBehaviour
         else
             _anim.animator.SetBool("walking", true);
 
+        // 입력받은 거에 따라 스프라이트 뒤집음
+        if (moveInput.x > 0)
+            _anim.sprite.flipX = true;
+        else if (moveInput.x < 0)
+            _anim.sprite.flipX = false;
 
-        rigid.AddForce(moveVelocity.normalized * moveSpeed);
+        // 이동
+        rigid.velocity = moveInput * moveSpeed;
     }
 
     bool RayCheck(Vector2 JBdirection)
